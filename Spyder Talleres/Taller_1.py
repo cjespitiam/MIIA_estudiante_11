b81524b47fdbefe458362a546123fb0143488b43
Tu Nombrecjespitiam <c.espitiam@uniandes.edu.co>
2021-09-06 20:30:31 -0500

    TALLER_1

--- Spyder Talleres/Taller_1.py
+++ Spyder Talleres/Taller_1.py
@@ -48,7 +48,45 @@ largo = len(nombres)
 
 import math
 
-bucle_1 = range()
-bucle_2 = range()
+bucle_1 = range(math.ceil(largo/2))
+bucle_2 = range(math.floor(largo/2))
+i_inverso = -1
 
-base_1 = 
+primera_mitad = []
+segunda_mitad = []
+
+for i in bucle_1:
+    primera_mitad.append(nombres[i])
+
+for i in bucle_2:
+    segunda_mitad.append(nombres[i_inverso])
+    i_inverso = i_inverso -1
+
+segunda_mitad.sort()
+
+######## CASO 3
+
+lista_abecedario = ["a", "b", "c", ["d", "e", ["f", "g"]]]
+
+lista_abc = [lista_abecedario[0],lista_abecedario[1],lista_abecedario[2]]
+
+lista_de = [lista_abecedario[3][0],lista_abecedario[3][1]]
+
+lista_fgh = [lista_abecedario[3][2][0],lista_abecedario[3][2][1],"h"]
+
+lista_a_j = lista_abc + lista_de + lista_fgh + ["i","j"]
+
+
+######## CASO 4
+
+
+tupla_primos =  (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97)
+
+a_buscar = 73
+
+indice = tupla_primos.index(a_buscar)
+
+lim_inferior = 3
+lim_superior = 53
+
+tupla_secuencia = tupla_primos[tupla_primos.index(lim_inferior):tupla_primos.index(lim_superior)+2:2]
